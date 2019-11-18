eadd941f24468a1f8bb366e248d2d5715aaf5fdd
Kobatai <taiga1983346@gmail.com>
2019-11-19 08:53:48 +0900

    add Request.php

--- application/core/Request.php
+++ application/core/Request.php
@@ -31,13 +31,15 @@ class Request
 
   public function getHost()
   {
+    // HTTPリクエストヘッダに含まれるホストの値を取得
     if (!empty($_SERVER['HTTP_HOST'])) {
       return $_SERVER['HTTP_HOST'];
     }
-
+    // なければApache側に設定されてホスト名を取得
     return $_SERVER['SERVER_NAME'];
   }
 
+  // HTTPSでアクセスされたか判定 その場合には$_SERVER['HTTPS']にonが入る
   public function isSsl()
   {
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
@@ -47,8 +49,24 @@ class Request
     return false;
   }
 
+  // ホスト部分以降のurlの値 URL制御を行うため
   public function getRequestUri()
   {
     return $_SERVER['REQUEST_URI'];
+  }
+
+  public function getBaseUrl()
+  {
+    $script_name = $_SERVER['SCRIPT_NAME'];
+
+    $request_uri = $this->getRequestUri();
+
+    if (0 === strpos($request_uri, $script_name)) {
+      return $script_name;
+    } else if (0 === strpos($request_uri,dirname($script_name))) {
+      return rtrim($script_name, '/');
+    }
+
+    return '';
   }
 }
